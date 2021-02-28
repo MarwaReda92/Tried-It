969933839b469f6cd72510d5be65e1b97dd10031
Mara <marwareda1992@hotmail.com>
2021-02-28 15:00:06 -0500

    Created a separate file for the header and included it in the index.php file

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tried It</title>
-</head>
-<body>
-<header>
-    <h1> Make Better Purchases Because We've Tried It!  </h1>
-</header>
+<?php
+include('header.php');
+?>
 <main>
     <form action="process.php" method="post">
         <input type="text" name="fname" placeholder="First Name">
         <input type="text" name="lname" placeholder="Last Name">
         <input type="email" name="email" placeholder="Email">
-        <input type="date" name="bday" placeholder="Date of Birth">
+        <label for="bday">Date of Birth:</label>
+        <input type="date" id="bday" name="bday">
         <input type="text" name="prof" placeholder="Profession">
         <input type="text" name="username" placeholder="Username">
         <input type="submit" value="submit" name="submit">
