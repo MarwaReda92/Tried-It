aa2c2008ec24ade2449775effa132c61ab7a8bf7
Mara <marwareda1992@hotmail.com>
2021-02-28 14:28:05 -0500

    Initial commit

---
+++ query.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar (20) NOT NULL,
+  `email` varchar (100) NOT NULL,
+  `birthday` date NOT NULL,
+  `profession` varchar(100) NOT NULL,
+  PRIMARY KEY (user_id)
+);
