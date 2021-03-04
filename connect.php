29c09eb6ccf6d1eb6f0ad419bde77c4dda085802
Mara <marwareda1992@hotmail.com>
2021-03-03 22:05:56 -0500

    added the database sql file

--- connect.php
+++ connect.php
@@ -1,11 +1,13 @@
 <?php
 try {
     //data source name (includes the type of data, hostname, and database name)
-    $dsn = 'mysql:host=localhost;dbname=tried_it';
+    $dsn = 'mysql:host=127.0.0.1;dbname=tried_it';
     $username = 'root';
     $password = '';
     $db = new PDO($dsn, $username, $password);
     echo "<p> Success </p>";
 } catch (PDOException $e) {
     echo "<p> Something went wrong </p>";
+    $error_message = $e->getMessage();
+    echo $error_message;
 }
