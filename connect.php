9274631c4e3bf1e3ba94fdce00f89b3367ef56f1
Mara <marwareda1992@hotmail.com>
2021-03-05 23:28:24 -0500

    users.sql file added

--- connect.php
+++ connect.php
@@ -1,11 +1,14 @@
 <?php
 try {
     //data source name (includes the type of data, hostname, and database name)
-    $dsn = 'mysql:host=127.0.0.1;dbname=tried_it';
-    $username = 'root';
-    $password = '';
+    $dsn = 'mysql:host=172.31.22.43;dbname=Marwa200394610';
+    $username = 'Marwa200394610';
+    $password = 'HB48IGQgTy';
+
     $db = new PDO($dsn, $username, $password);
+
     echo "<p> Success </p>";
+
 } catch (PDOException $e) {
     echo "<p> Something went wrong </p>";
     $error_message = $e->getMessage();
